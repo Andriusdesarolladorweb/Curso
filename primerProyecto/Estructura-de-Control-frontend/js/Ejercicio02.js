6a945f06cd25f476fe26adaf465c7a7014034ebf
Andrius Rosa Castro <andriusrosacastro2@gmail.com>
2022-10-26 17:30:01 +0100

    Termine el ejercicio de js

--- primerProyecto/Estructura-de-Control-frontend/js/Ejercicio02.js
+++ primerProyecto/Estructura-de-Control-frontend/js/Ejercicio02.js
@@ -3,9 +3,10 @@ let alturacentrimetros = 5,
 // Mi altura en metro en flotante
     alturametro = 5.1;
 // Mi peso en kilogramos en flotante
-     pesokilogramos = 160.0555555550
+     pesokilogramos = 160.0555555550,
 // Mi peso en kilogramos redondeado hacia abajo
-
+redondeado_hacia_abajp = Math.floor(pesokilogramos),
 // Mi peso en kilogramos redondeado hacia ariba
-   
-    
+  redondeado_hacia_arriba = Math.ceil(pesokilogramos),
+//máximo valor que se puede obtener en Javascript + 1
+max_valor_js = Number.MAX_VALUE + 1;  
