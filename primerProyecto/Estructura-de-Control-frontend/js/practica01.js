e192f81b77e176015ff06f63e9212f02a32e1b56
Andrius Rosa Castro <andriusrosacastro2@gmail.com>
2022-10-28 14:30:16 +0100

    Concatenar y obtener framento de otra lista

--- primerProyecto/Estructura-de-Control-frontend/js/practica01.js
+++ primerProyecto/Estructura-de-Control-frontend/js/practica01.js
@@ -4,8 +4,12 @@ let array = ["Lunes",
              "marte",
              "miercoles"]
 
+let numeros = [1, 2, 3]
+            
+
 // acendiendo al array
 console.log(array);
+
 
 // agregar al array en el final 
 array.push("Jueves", "viernes", sabado , false);
@@ -20,10 +24,24 @@ array.pop();
 array.shift();
 
 //metodo para eliminar 
-array.splice(0, 1)
+array.splice(0, 1);
 
 //metodo para  agregar
-array.splice(0, 0, "lunes", 05)
+array.splice(0, 0, "lunes");
 
 // metodo para  modificar 
-array.slice(0, 2, 02)
+array.slice(0, 2, 02);
+
+// como unir dos lista 
+let juntar_lista = numeros.concat(array);
+
+// como unir dos lista  el mas usado 
+let juntar_lista01 = [...array, ...numeros];
+console.log(juntar_lista01);
+
+// como tener  una lista aparti de otra lista 
+const oterne = array.slice(2, 4);
+console.log(oterne);
+
+
+
