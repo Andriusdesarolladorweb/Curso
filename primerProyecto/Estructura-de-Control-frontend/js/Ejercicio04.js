f39342ad081a9515d306685b693ec0805acaae07
Andrius Rosa Castro <andriusrosacastro2@gmail.com>
2022-10-28 17:18:25 +0100

    comenze a ser mi ejercio pero me falta

---
+++ primerProyecto/Estructura-de-Control-frontend/js/Ejercicio04.js
@@ -0,0 +1,13 @@
+// lista de la compra
+const Compra = ["Arroz", "Agua", "Pan", "Tomate", "Leche" ];
+
+//Modifica la lista de la compra Añadiendo 
+const Añadir = Compra.push("Aceite de Girasol");
+
+//modificar la lista de la compra  elimindo
+const eliminar = Compra.pop();
+
+console.log(Compra);
+
+//Lista de mi peliculas favoritas 
+
