eab465802b940836baa3914ad4345429c2471bfb
Your Name <you@example.com>
2022-12-30 09:37:32 -0400

    si

---
+++ primerProyecto/Estructura-de-Control-frontend/js/objetos.js
@@ -0,0 +1,41 @@
+// Mi datos personales 
+const datospersonales = {
+    Nombre: "Andrius",
+    Apellido: "Rosa Castro",
+    Edad: 17,
+    Altura: 5.7,
+    EresDesarollador: true
+}
+
+// variable con mi edad 
+const MiEdad = Edad;
+
+const mejoresAmigas = [ 
+  
+
+
+]
+
+// lista con mi objeto datospersonales y tambien con el mejoresAmigas
+const lista = [
+    datospersonales,
+
+    {
+        Nombre: "Camila",
+        Apellido: "carrion",
+        Edad: 18,
+        Altura: 5.1,
+        EresDesarollador: false
+        
+    },
+
+    {
+        Nombre: "Diorkys",
+        Apellido: "Cabrera",
+        Edad: 17,
+        Altura: 5.8,
+        EresDesarollador: true
+        
+    }
+]
+
