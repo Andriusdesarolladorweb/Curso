2171bffdfd9592ad9bef9d308403a2e85183c078
Andriusdesarolladorweb <112970410+Andriusdesarolladorweb@users.noreply.github.com>
2023-01-20 22:47:41 -0400

    Update index.js
    Cambie algo

--- modulos-es6/index.js
+++ modulos-es6/index.js
@@ -1,11 +1,4 @@
 import * as moduloController from './modulos/Controller.js'
 import axios from "chalk"
 
-const error = chalk.bold.red;
-const warning = chalk.hex('#FFA500'); // Orange color
-
-console.log(error('Error!'));
-console.log(warning('Warning!'));
-
-const mul = moduloController.Multiplica(1,2);
-const sum = moduloController.suma(4,5);
+console.log(chalk.green(multiplica(suma(1, 2), suma(4, 5))))
